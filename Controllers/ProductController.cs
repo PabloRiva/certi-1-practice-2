90f0852c04ae6270431e0a65cc6fd0b2aeaf9f8f
Pablo Riva <pabloarivaponce@gmail.com>
2022-05-08 07:52:21 -0400

    Added swagger to the project

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Logic;
+using Logic.Managers;
+
+namespace practice3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        
+
+        public ProductController()
+        {
+          
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            ProductManager productMngr = new ProductManager();
+            return Ok(productMngr.GetProducts());
+        }
+    }
+}
